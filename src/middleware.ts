41e29f2501a546a02dcc0483b74b8cde7f6e5a33
Stephan Kulla <git.mail@kulla.me>
2024-07-05 21:08:36 +0200

    feat: Add ltijs to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,12 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { Provider as ltijs } from 'ltijs'
+
+ltijs.setup(
+  'LTIKEY',
+  { url: 'mongodb://localhost:27017/lti' },
+  { cookies: { secure: false, sameSite: '' } },
+)
 
 export function middleware(request: NextRequest) {
   return NextResponse.json({
